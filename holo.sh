b6c1a875ecbed3390d68e126654602d909568545
TimLogan0106 <147916992+TimLogan0106@users.noreply.github.com>
2024-02-06 11:58:51 +0700

    Update holo.sh

--- holo.sh
+++ holo.sh
@@ -1,9 +1,6 @@
 sudo apt-get update && apt-get install qemu -y
-sudo apt install qemu-utils -y
-sudo apt install qemu-system-x86-xen -y
-sudo apt install qemu-system-x86 -y
+sudo apt install qemu-system -y
 qemu-img create -f raw win11.img 512G
 wget -O win11.iso 'https://cd2.holoiso.ru.eu.org/holoiso-installer/holoiso_beta_snapshot20240125.1712.26-INSTALLER-06-x86_64.iso'
-
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 -m 8G -cpu host -boot order=c -drive file=win11.iso,media=cdrom -drive file=win11.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -enable-kvm
