06466f7bc08d0b3232a4151464522712f16ee39c
TimLogan0106 <147916992+TimLogan0106@users.noreply.github.com>
2024-02-06 12:01:45 +0700

    Update holo.sh

--- holo.sh
+++ holo.sh
@@ -1,5 +1,5 @@
 sudo apt-get update && apt-get install qemu -y
-sudo apt install qemu-system -y
+sudo apt install qemu-system-x86_64 -y
 qemu-img create -f raw win11.img 512G
 wget -O win11.iso 'https://cd2.holoiso.ru.eu.org/holoiso-installer/holoiso_beta_snapshot20240125.1712.26-INSTALLER-06-x86_64.iso'
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
