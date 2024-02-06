3e0cedf9c2c53609eb48e09537708e80bcbf6bc3
TimLogan0106 <147916992+TimLogan0106@users.noreply.github.com>
2024-02-06 11:16:27 +0700

    Update holo.sh

--- holo.sh
+++ holo.sh
@@ -1,23 +1,17 @@
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip &>/dev/null &
-unzip ngrok-stable-linux-amd64.zip1 &>/dev/null &
-read -p "Ctrl + V Authtoken: " CRP 
-./ngrok authtoken $CRP 
-nohup ./ngrok tcp 3388 &>/dev/null &
-./ngrok tcp 3388 &>/dev/null &
 sudo apt-get update && apt-get install qemu -y
 sudo apt install qemu-utils -y
 sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
-qemu-img create -f raw Ubuntu.img 64G
-wget -O Ubuntu.iso 'https://cd2.holoiso.ru.eu.org/holoiso-installer/holoiso_beta_snapshot20240125.1712.26-INSTALLER-06-x86_64.iso'
+qemu-img create -f raw windows7nano.img 64G
+wget -O windows7_super-nano_lite.iso 'https://cd2.holoiso.ru.eu.org/holoiso-installer/holoiso_beta_snapshot20240125.1712.26-INSTALLER-06-x86_64.iso'
+
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
   -m 8G \
   -cpu EPYC \
   -boot order=d \
-  -drive file=Ubuntu.iso,media=cdrom \
-  -drive file=Ubuntu.img,format=raw \
+  -drive file=windows7_super-nano_lite.iso,media=cdrom \
+  -drive file=windows7nano.img,format=raw \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
   -vnc :0 \
@@ -25,6 +19,3 @@ sudo qemu-system-x86_64 \
   -smp cores=4 \
   -device rtl8139,netdev=n0 -netdev user,id=n0 \
   -accel tcg,thread=multi \
-
-
-  
